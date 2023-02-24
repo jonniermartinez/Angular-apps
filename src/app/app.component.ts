a938d082d7b4871799760564be629c4e6b721777
Jonnier <jonnierosjitas@gmail.com>
2023-02-24 11:07:46 -0500

    app terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'dados-app';
+  dadoIzq = '../assets/img/dice1.png';
+  dadoDer = '../assets/img/dice4.png';
+  numero1: number = 2;
+  numero2: number = 4;
+
+  tirarDados(): void {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+
+    this.dadoIzq = `../assets/img/dice${this.numero1}.png`;
+    this.dadoDer = `../assets/img/dice${this.numero2}.png`;
+  }
 }
